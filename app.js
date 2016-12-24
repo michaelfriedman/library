776a2cc746a706317a20fd4848dee5933cb68a70
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-24 09:15:44 -0800

    Initial project setup with express and bower.

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+const port = 5000;
+
+app.use(express.static('public'));
+
+app.use(express.static('src/views'));
+
+app.get('/', (req, res) => {
+  res.send('Hello world.')
+});
+
+app.get('/books', (req, res) => {
+  res.send('Hello books.')
+});
+
+app.listen(5000, (err) => {
+  console.log(`Running server on port: ${port}`);
+});
