05ca38b87850b7f4d324526b7c35e0f3f0731c2c
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-27 12:52:29 -0800

    Setting up EJS.

--- app.js
+++ app.js
@@ -2,16 +2,13 @@
 
 const express = require('express');
 
-const handlebars = require('express-handlebars');
-
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.set('views', './src/views');
-app.engine('.hbs', handlebars({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Hello from render.', list: ['a', 'b'] });
