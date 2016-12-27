f490323b5ba1b36176f43776be82ef74f0ee78d1
Michael Friedman <contact@michaeldavidfriedman.com>
2016-12-27 14:11:01 -0800

    Route for books page successfully routing GET requests. List of books now dynamically rendering to page using EJS templating.

--- app.js
+++ app.js
@@ -6,10 +6,66 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 5000;
+const bookRouter = express.Router();
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+const books = [
+  {
+    title: 'Chaos Monkeys',
+    genre: 'Biographies & Memoirs',
+    author: 'Antonio Garcia Martinez',
+    read: true
+  },
+  {
+    title: 'The Tipping Point: How Little Things Can Make a Big Difference',
+    genre: 'Non-Fiction',
+    author: 'Malcolm Gladwell',
+    read: true
+  },
+  {
+    title: 'Way of the Peaceful Warrior',
+    genre: 'Personal Development',
+    author: 'Dan Millman',
+    read: true
+  },
+  {
+    title: 'Harry Potter and the Philosopher\'s Stone',
+    genre: 'Fantasy',
+    author: 'J. K. Rowling',
+    read: false
+  },
+  {
+    title: 'The Hunger Games',
+    genre: 'Fantasy',
+    author: 'Suzanne Collins',
+    read: false
+  },
+  {
+    title: 'The Pelican Brief: A Novel',
+    genre: 'Fiction: Legal Thriller',
+    author: 'John Grisham',
+    read: false
+  },
+];
+
+bookRouter.route('/')
+  .get((req, res) => {
+    res.render('books', { title: 'Books', nav: [
+      { Link: '/Books', Text: 'Books' },
+      { Link: '/Authors', Text: 'Authors' }
+    ],
+  books: books });
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('Hello Single Book');
+  });
+
+app.use('/Books', bookRouter);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Hello from render', nav: [
